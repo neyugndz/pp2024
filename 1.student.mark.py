4119172c0e7f915d54e7f5dd4d97aac4597f36d4
Dieu Anh Nguyen <dangnguyen180904@gmail.com>
2024-01-17 17:17:14 +0700

    Lab 2 Update input and list method

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,30 +1,120 @@
-def num_students():
-    global num_of_stu 
-    num_of_stu = int(input("Enter the number of the students: "))
-    print("The total number of student is: " + str(num_of_stu))   
+def input_something(args, items):
+    num = int(input(f"Enter the number of {args}: "))
+    for i in range(num):
+        items.append(input_infos(args))
+    return items
 
-def student_info():
-    id, name, dob = input("Enter student ID, name and date of birth(dd/mm/yyyy): ").split();
-    print("Student ID: " + id)
-    print("Name: " + name)
-    print("Date of birth:(dd/mm/yyyy) "+ dob)
+def input_infos(args):
+    item = {}
+    if args == "students":
+        id, name, dob = input("Enter student ID, name and date of birth(dd/mm/yyyy): ").split()
+        item["id"] = id
+        item["name"] = name
+        item["DoB"] = dob
+    elif args == "courses":
+        id, name = input("Enter course ID and name: ").split()
+        item["id"] = id
+        item["name"] = name
+    return item
 
-def num_courses():
-    global num_of_courses
-    num_of_courses = int(input("Enter the number of the courses: "))
-    print("The total number of courses is: " + str(num_of_courses))
+def list_students(students):
+    if len(students) == 0:
+        print("There aren't any students yet")
+    else:
+        print("Here is the students list: ")
+        
+        for i,student in enumerate(students, start=0): #enumerate: to print the list with variables along with their index in the loop
+            print(f"{i+1}. {student['id']} - {student['name']} - {student['DoB']}")
+            
+def list_courses(courses):
+    if len(courses) == 0:
+        print("There aren't any courses yet")
+    else:
+        print("Here is the courses list: ")
+        
+        for i,course in enumerate(courses,start=0):
+            print(f"{i+1}. {course['id']} - {course['name']}")           
 
-def course_info():
-    id, name = input("Enter course ID and name: ").split()
-    print("Course ID: " + id)
-    print("Name of the course: " + name)
+def input_marks(students, courses, marks):
+    if len(courses) == 0 or len(students) == 0:
+        print("There must be courses and students before inputting marks.")
+        return marks
+    print("Select a course to input marks: ")
+    list_courses(courses)
+    course_index = int(input("Enter the course number: ")) - 1
     
-num_students()
-
-for i in range(num_of_stu):
-    student_info()
+    if course_index < 0 or course_index >= len(courses):
+        print("Invalid course selection.")
+        return marks
     
-num_courses()
+    selected_course = courses[course_index]
+    
+    print(f"Input marks for students in course {selected_course['name']}:")
+    for student in students:
+        mark = float(input(f"Enter marks for {student['name']}: "))
+        marks.append({
+            'student_id': student['id'],
+            'course_id': selected_course['id'],
+            'mark': mark
+        })
+    return marks
+        
+def show_student_marks(marks, courses, students):
+    if len(courses) == 0 or len(students) == 0 or len(marks) == 0:
+        print("There must be courses, students, and marks to show student marks.")
+        return
+    
+    print("Select a course to show student marks: ")
+    list_courses(courses)
+    course_index = int(input("Enter the course number: ")) - 1
+    
+    if course_index < 0 or course_index >= len(courses):
+        print("Invalid course selection.")
+        return 
+    
+    selected_course = courses[course_index]
+    
+    print(f"Student marks for course {selected_course['name']}: ")
+    for mark in marks:
+        if mark['course_id'] == selected_course['id']:
+            student = next((student for student in students if student['id'] == mark['student_id']), None)   
+            if student:
+                print(f"{student['name']} - Marks: {mark['mark']}")
 
-for i in range(num_of_courses):
-    course_info()
+
+def main():
+    students = []
+    courses = []
+    marks = []
+    while(True):
+        print("""
+    0. Exit
+    1. Input students
+    2. Input courses
+    3. Input marks for a course
+    4. List students
+    5. List courses
+    6. Show student marks for a course
+    """)
+        options = int(input("Your choice: "))
+        
+        if options == 0:
+            break
+        elif options == 1:
+            students = input_something("students",students)
+        elif options == 2:
+            course = input_something("courses",courses)
+        elif options == 3:
+            marks = input_marks(students, courses, marks)
+        elif options == 4:
+            list_students(students)
+        elif options == 5:
+            list_courses(courses)
+        elif options == 6:
+            show_student_marks(marks, courses, students)
+        else:
+            print("Invalid input. Please try again")
+            
+            
+if __name__ == "__main__":
+    main()
---
+++ 2.student.mark.py
@@ -0,0 +1,140 @@
+class Student:
+    def __init__(self):
+        self.__student_id = input("Enter the student's id: ")
+        self.__name = input("Enter the student's name: ")
+        self.__dob = input("Enter the student's dob: ")
+        
+    def get_id(self):
+        return self.__student_id
+
+    def get_name(self):
+        return self.__name
+    
+    def get_dob(self):
+        return self.__dob
+
+class Course:
+    def __init__(self):
+        self.__id = input("Enter the course's id: ")
+        self.__name = input("Enter the course's name: ")
+        
+    def get_id(self):
+        return self.__id
+
+    def get_name(self):
+        return self.__name
+    
+class Utils:
+    def input_something(args):
+        return int(input(f"Enter the number of {args}: "))
+ 
+class University:
+    def __init__(self):
+        self.__students = []
+        self.__courses = []
+        self.__marks = []
+        self.__num_students = 0
+        self.__num_courses = 0
+    
+    def get_num_students(self):
+        return self.__num_students
+    
+    def get_num_courses(self):
+        return self.__courses
+    
+    def get_student(self):
+        return self.__students
+    
+    def get_courses(self):
+        return self.__courses
+    
+    def set_num_students(self):
+        self.__num_students = Utils.input_something("students")
+        
+    def set_num_courses(self):
+        self.__num_courses = Utils.input_something("courses")
+    
+    def set_student(self):
+        self.set_num_students()
+        for _ in range (self.__num_students):
+            student = Student()
+            self.__students.append(student)
+    
+    def set_courses(self):
+        self.set_num_courses()
+        for _ in range (self.__num_courses):
+            course = Course()
+            self.__courses.append(course)
+            
+    def list_students(self):
+        if len(self.__students) == 0:
+            print("There aren't any students yet.")
+        else:
+            print("Here is the student list: ")
+            
+            for i, student in enumerate(self.__students,start=1):
+                print(f"{i}. ID: {student.get_id()} - Name: {student.get_name()} - DoB: {student.get_dob()}")
+    
+    def list_courses(self):
+        if len(self.__courses) == 0:
+            print("There aren't any courses yet.")
+        else:
+            print("Here is the course list")
+            
+            for i, course in enumerate(self.__courses,start=1):
+                print(f"{i}. ID: {course.get_id()} - Name: {course.get_name()}")
+    
+    def input_marks(self):
+        if len(self.__courses) == 0 or len(self.__students) == 0:
+            print("There must be courses and students before inputting marks.")
+            return
+        print("Select a course to input marks: ")
+        self.list_courses()
+        course_index = int(input("Enter the course number: ")) - 1
+        
+        if course_index < 0 or course_index >= len(self.__courses):
+            print("Invalid course selection. ")
+            return
+        
+        selected_course = self.__courses[course_index]
+        
+        print(f"Input marks for students in course {selected_course.get_name()}")
+        
+        for student in self.__students:
+            mark = float(input(f"Enter marks for {student.get_name()}: "))
+            self.__marks.append({
+                'Student ID': student.get_id(),
+                'Course ID': selected_course.get_id(),  
+                'Marks': mark
+            })
+        
+def main():
+    univ = University()
+    while True:
+        print("""
+    0. Exit
+    1. Add Students
+    2. Add Courses
+    3. Input marks
+    4. List Students
+    5. List Courses
+    """)
+        option = int(input("Your choice: "))
+        if option == 0:
+            break
+        elif option == 1:
+            univ.set_student()
+        elif option == 2:
+            univ.set_courses()
+        elif option == 3:
+            univ.input_marks()
+        elif option == 4:
+            univ.list_students()
+        elif option == 5:
+            univ.list_courses()
+        else:
+            print("Invalid input. Please try again")
+
+if __name__ == "__main__":
+    main()
+    
